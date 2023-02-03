99c812d6ad710e06e76a499ae45bbc0969d80818
yusufbay1 <83879572+yusufbay1@users.noreply.github.com>
2023-02-03 14:33:19 +0300

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,1 @@
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
-$font-family: 'Poppins', sans-serif;
-$color-c1: #ecebf6;
-$color-c2: #272c48;
-$color-c3: #d4357d;
-$color-c4: #8a8ba1;
-$color-c5: #ECB332;
-$color-c6: #27B8BF;
+
